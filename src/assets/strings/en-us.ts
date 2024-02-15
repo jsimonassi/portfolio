0db876ac9594838aaf99913ef12303dae2c002fd
João Victor Simonassi <jsimonassi@id.uff.br>
2024-02-15 09:48:17 -0300

    Update en-us.ts

--- src/assets/strings/en-us.ts
+++ src/assets/strings/en-us.ts
@@ -3,7 +3,7 @@ export const EN_US_MESSAGES = {
 	projects: "Projects",
 	resume: "Resume",
 	contact: "Contact",
-	about: "Full Stack Developer at Radix Engineering and Software and graduated in Computer Science from the Federal Fluminense University. I work on React Native, ReactJS and Native Android apps projects. I love working on applications that are in direct contact with the end user. Seeing my solutions contributing and impacting people's lives is extremely gratifying.",
+	about: "Mobile Developer at Radix Engenharia e Software and graduated in Computer Science from Universidade Federal Fluminense. I work mainly on React Native projects. I also work on ReactJS and Native Android projects. I love working on applications that are in direct contact with the end user. Seeing my solutions contributing and impacting people's lives is extremely rewarding.",
 	showMyProjects: "Check out my projects!",
 	otherProjects: "Other personal projects are available at:",
 	downloadInstructions: "Download the resume in PDF to access more information.",
