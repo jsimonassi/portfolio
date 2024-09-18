86e6d5694c52e9883cffde4472216e1c913f1d86
jsimonassi <jsimonassi@id.uff.br>
2024-09-17 23:06:52 -0300

    Remove unused import

--- src/screens/MainPage/components/Contact/index.tsx
+++ src/screens/MainPage/components/Contact/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { ContactBox, Container, InfoBox } from "./styles";
 import { useLanguage } from "../../../../contexts/language";
-import linkedinLogo from "../../../../assets/images/linkedinLogo.png";
-// import emailLogo from "../../../../assets/email.png";
 
 const Contact = () => {
 
